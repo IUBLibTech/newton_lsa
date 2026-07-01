6288d84ff09a3f183fa7282ffab70c1deee95351
wehooper <whooperx@gmail.com>
2026-07-01 01:14:10 -0700

    added Toggle Highlighing button to side by side view

--- displaycorrs.php
+++ displaycorrs.php
@@ -12,26 +12,26 @@ function & prepstring($strInput)
 	return $str2;
 }
 
-function & translateGentium($strInput)
-{
-	// $newtonSansTestInts = [ 57349 => 57349, 128882 => 128882, 128815 => 128815, 128840 => 128840, 128774 => 128774, 128769 => 128769, 128770 => 128770, 128771 => 128526, 128772 => 128772, 128773 => 128773, 128774 => 128774, 128775 => 128775, 128776 => 128776, 128778 => 128778, 128779 => 128779, 128780 => 128780, 128781 => 128781, 128784 => 128784, 128785 => 128785, 128788 => 128788, 128789 => 128789, 128790 => 128790, 128791 => 128791, 128796 => 128796, 128797 => 128797, 128799 => 128799, 128800 => 128800, 128801 => 128801, 128802 => 128802, 128805 => 128805, 128806 => 128806, 128807 => 128807, 128808 => 128808, 128809 => 128809, 128810 => 128810, 128811 => 128811, 128812 => 128812, 128813 => 128813, 128814 => 128814, 128816 => 128816, 128817 => 128817, 128818 => 128818, 128819 => 128819, 128820 => 128820, 128821 => 128821, 128825 => 128825, 128830 => 128830, 128831 => 128831, 128838 => 128838, 128846 => 128846, 128847 => 128847, 128848 => 128848, 128849 => 128849, 128850 => 128850, 128853 => 128853, 128862 => 128862, 128871 => 128871, 128875 => 128875, 128878 => 128878, 128880 => 128880, 128883 => 128883 ];
-	$gentium2sans = [ 57344 => 57349, 57352 => 128882, 57367 => 128815, 57370 => 128840, 57434 => 128774, 58113 => 128769, 58114 => 128770, 58115 => 128771, 58116 => 128772, 58117 => 128773, 58118 => 128774, 58119 => 128775, 58120 => 128776, 58122 => 128778, 58123 => 128779, 58124 => 128780, 58125 => 128781, 58128 => 128784, 58129 => 128785, 58132 => 128788, 58133 => 128789, 58134 => 128790, 58135 => 128791, 58140 => 128796, 58141 => 128797, 58143 => 128799, 58144 => 128800, 58145 => 128801, 58146 => 128802, 58149 => 128805, 58150 => 128806, 58151 => 128807, 58152 => 128808, 58153 => 128809, 58154 => 128810, 58155 => 128811, 58156 => 128812, 58157 => 128813, 58158 => 128814, 58160 => 128816, 58161 => 128817, 58162 => 128818, 58163 => 128819, 58164 => 128820, 58165 => 128821, 58169 => 128825, 58174 => 128830, 58175 => 128831, 58182 => 128838, 58190 => 128846, 58191 => 128847, 58192 => 128848, 58193 => 128849, 58194 => 128850, 58197 => 128853, 58206 => 128862, 58215 => 128871, 58219 => 128875, 58222 => 128878, 58224 => 128880, 58227 => 128883 ];
+// function & translateGentium($strInput)
+// {
+// 	// $newtonSansTestInts = [ 57349 => 57349, 128882 => 128882, 128815 => 128815, 128840 => 128840, 128774 => 128774, 128769 => 128769, 128770 => 128770, 128771 => 128526, 128772 => 128772, 128773 => 128773, 128774 => 128774, 128775 => 128775, 128776 => 128776, 128778 => 128778, 128779 => 128779, 128780 => 128780, 128781 => 128781, 128784 => 128784, 128785 => 128785, 128788 => 128788, 128789 => 128789, 128790 => 128790, 128791 => 128791, 128796 => 128796, 128797 => 128797, 128799 => 128799, 128800 => 128800, 128801 => 128801, 128802 => 128802, 128805 => 128805, 128806 => 128806, 128807 => 128807, 128808 => 128808, 128809 => 128809, 128810 => 128810, 128811 => 128811, 128812 => 128812, 128813 => 128813, 128814 => 128814, 128816 => 128816, 128817 => 128817, 128818 => 128818, 128819 => 128819, 128820 => 128820, 128821 => 128821, 128825 => 128825, 128830 => 128830, 128831 => 128831, 128838 => 128838, 128846 => 128846, 128847 => 128847, 128848 => 128848, 128849 => 128849, 128850 => 128850, 128853 => 128853, 128862 => 128862, 128871 => 128871, 128875 => 128875, 128878 => 128878, 128880 => 128880, 128883 => 128883 ];
+// 	$gentium2sans = [ 57344 => 57349, 57352 => 128882, 57367 => 128815, 57370 => 128840, 57434 => 128774, 58113 => 128769, 58114 => 128770, 58115 => 128771, 58116 => 128772, 58117 => 128773, 58118 => 128774, 58119 => 128775, 58120 => 128776, 58122 => 128778, 58123 => 128779, 58124 => 128780, 58125 => 128781, 58128 => 128784, 58129 => 128785, 58132 => 128788, 58133 => 128789, 58134 => 128790, 58135 => 128791, 58140 => 128796, 58141 => 128797, 58143 => 128799, 58144 => 128800, 58145 => 128801, 58146 => 128802, 58149 => 128805, 58150 => 128806, 58151 => 128807, 58152 => 128808, 58153 => 128809, 58154 => 128810, 58155 => 128811, 58156 => 128812, 58157 => 128813, 58158 => 128814, 58160 => 128816, 58161 => 128817, 58162 => 128818, 58163 => 128819, 58164 => 128820, 58165 => 128821, 58169 => 128825, 58174 => 128830, 58175 => 128831, 58182 => 128838, 58190 => 128846, 58191 => 128847, 58192 => 128848, 58193 => 128849, 58194 => 128850, 58197 => 128853, 58206 => 128862, 58215 => 128871, 58219 => 128875, 58222 => 128878, 58224 => 128880, 58227 => 128883 ];
 
-	$inputList = mb_str_split($strInput, 1, 'UTF-8');
-	$outputList = array();
-	foreach ($inputList as $char) {
-		$charCode = mb_ord($char, 'UTF-8');  // returns a decimal int
-		if (array_key_exists($charCode, $gentium2sans)) {
-			$newCharCode = $gentium2sans[$charCode];
-			$outputList[] = mb_chr($newCharCode, 'UTF-8');
-		} else {
-			$outputList[] = $char;
-		}
-	}
-	$strOutput = implode('', $outputList);
+// 	$inputList = mb_str_split($strInput, 1, 'UTF-8');
+// 	$outputList = array();
+// 	foreach ($inputList as $char) {
+// 		$charCode = mb_ord($char, 'UTF-8');  // returns a decimal int
+// 		if (array_key_exists($charCode, $gentium2sans)) {
+// 			$newCharCode = $gentium2sans[$charCode];
+// 			$outputList[] = mb_chr($newCharCode, 'UTF-8');
+// 		} else {
+// 			$outputList[] = $char;
+// 		}
+// 	}
+// 	$strOutput = implode('', $outputList);
 	
-	return $strOutput;
-}
+// 	return $strOutput;
+// }
 		
 
 // function from PHP manual by Jesse Bussman at gmail
@@ -155,15 +155,6 @@ $doc2title = $doc2data[1];
 $doc2mstitle = $doc2data[2];
 $d2string = $doc2data[3];
 
-// change the font to get correct NewtonSans characters if data was encoded for Gentium Newton
-// if ($gentium) {
-// 	$d1string = translateGentium($d1stringIn);
-// 	$d2string = translateGentium($d2stringIn);
-// } else {
-// 	$d1string = $d1stringIn;
-// 	$d2string = $d2stringIn;	
-// }
-
 /*********************************************************
   Form MySQL query for list of terms for matching 
 *********************************************************/
@@ -222,6 +213,9 @@ header('Content-Type: text/html; charset=utf-8');
 			color: firebrick;
 			<!-- background-color: yellow; -->
 		}
+		.match.no_highlight {
+			color: black;
+		}
 		.content {
 			font-size:small;
 		}
@@ -231,12 +225,23 @@ header('Content-Type: text/html; charset=utf-8');
 <body>
 <!-- Newton Skin -->
 <?php require_once("design/uniform-title.php"); ?>
+<script type="text/javascript">
+function toggleMatches() {
+    const matches = document.querySelectorAll('span.match');
+	// console.log(matches);
 
+    matches.forEach(element => {
+        element.classList.toggle('no_highlight');
+    })
+	// alert("after toggle");
+}
+</script>
 <table class="content" style="margin-left: 10px; font-family: 'GentiumNewton'; font-size: 16px; width: 100%;">
+<tr><td width="40%">&nbsp;&nbsp;<input type="button" id="toggle-highlight-button" value="Toggle highlighting" onclick="toggleMatches()" style="float: right"></td></tr>
 <tr>
 <td width="40%" valign="top">
 <?php 
-echo "Open the diplomatic folio: <a href='".$doc1url."' target='_blank'>".$doc1url." (new window)</a><br/><br/>";
+echo "See diplomatic folio (w/image): <a href='".$doc1url."' target='_blank'>".$doc1url." (new window)</a><br/><br/>";
 echo "$doc1alch<br/>$doc1title<br/>$doc1mstitle<br/><br/>";
 foreach($d1 as $line1) {
 	if ($line1 == "") {
@@ -254,8 +259,6 @@ foreach($d1 as $line1) {
 		}
 		$w1x = prepstring($w1);
 		if (in_array($w1x, $terms) && in_array($w1x, $d2check)) {
-		//if (in_array($w1x, $terms) && strpos($d2sx, $w1x)>0) {
-			//if (in_array($wx1, $terms) && preg_match("/\s$wx1\s/", $d2sx)) {
 			print("<span class=\"match\">$w1</span> ");
 		}
 		else {
@@ -268,7 +271,7 @@ foreach($d1 as $line1) {
 </td>
 <td width="40%" valign="top">
 <?php 
-echo "Open the diplomatic folio: <a href='".$doc2url."' target='_blank'>".$doc2url." (new  window)</a><br/></br>";
+echo "See diplomatic folio (w/image): <a href='".$doc2url."' target='_blank'>".$doc2url." (new  window)</a><br/></br>";
 echo "$doc2alch<br/>$doc2title<br/>$doc2mstitle<br/><br/>";
 foreach($d2 as $line2) {
 	if ($line2 == "") {
